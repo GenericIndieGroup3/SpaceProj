6fea8e512fd1fda25ca35804268ba2d47ca5be7c
rucraft42 <rucraft42@gmail.com>
2015-02-10 09:57:13 -0800

    Minor Tweaks
    Set some variables private in the Vector2 class. Added the methods I
    wrote to the PhysicsObject class. We can choose which ones to keep and
    comment out the extra later.

--- src/PhysicsObject.java
+++ src/PhysicsObject.java
@@ -5,7 +5,7 @@ public class PhysicsObject {
 	//Unfortunately, this is Java, so get/set methods for below vars
 	private Vector2 position;
 	private Vector2 velocity;
-	private Vector2 acceleration = new Vector2(0, 0);
+	private Vector2 acceleration = new Vector2();
 	
 	private double gravitationMass;
 	private double inertialMass;
@@ -18,23 +18,44 @@ public class PhysicsObject {
 	}
 	
 	public PhysicsObject(Vector2 position, double mass){
-		this(position, new Vector2(0, 0), mass, mass);
+		this(position, new Vector2(), mass, mass);
 	}
 	
-	public Vector2 calculateAcceleration(Vector2 force){
+	private Vector2 calculateAcceleration(Vector2 force){
 		return force.multiply( 1 / inertialMass);
 	}
+	
 	public void addForce(Vector2 force){
 		Vector2 deltaAcceleration = calculateAcceleration(force);
 		acceleration.setAdd(deltaAcceleration);
+	}
+	
+	//My Version
+	private Vector2 addForces(Vector2[] forces){
+		Vector2 netForce = new Vector2();
+		for(int i=0;i<forces.length;i++){
+			netForce.setAdd(forces[i]);
+		}
+		return netForce;
 	}
 	
 	public void update(){
 		velocity.setAdd(acceleration);
 		//So I reset acceleration every update cycle
 		//Is this how we should do it or store forces somehow or what?
-		acceleration.setEquals(new Vector2(0, 0));
+		/* The way I did it is you pass force into update, and update
+		 * calculates acceleration. Neither acceleration nor force is stored
+		 */
+		acceleration.setEquals(new Vector2());
 		position.setAdd(velocity);
+	}
+	
+	//My Version
+	public void update(Vector2[] forces){
+		Vector2 netForce = addForces(forces);
+		Vector2 acceleration = calculateAcceleration(netForce);
+		position.setAdd(velocity);
+		velocity.setAdd(acceleration);
 	}
 	
 	public Vector2 getPosition(){return position;}
