556c69b94f8f3e5f8cfdb6c5bba0c1ebdcf013cd
rucraft42 <rucraft42@gmail.com>
2015-02-10 13:00:41 -0800

    Started Physics Engine
    Added gravity force calculator. WARNING: returns one out of 2 forces.

--- src/Physics.java
+++ src/Physics.java
@@ -1,9 +1,23 @@
 import java.util.*;
 
 public class Physics {
+	private static final int GRAVITATIONAL_CONSTANT = 3;
+	
 	private List<PhysicsObject> obj = new ArrayList<PhysicsObject>();
 	public Physics(){
 		
+	}
+	
+	public void addObj(PhysicsObject o){
+		obj.add(o);
+	}
+	
+	//TODO Will return a one-way vector. Need to figure out force buffer and optimize for 2 objects
+	private Vector2 getGrav(PhysicsObject a, PhysicsObject b){
+		Vector2 distance = b.getPosition().subtract(a.getPosition());
+		double distanceMag = distance.magnitude();
+		double gravForceMag = GRAVITATIONAL_CONSTANT * a.getGravitationalMass() * b.getGravitationalMass() / (distanceMag * distanceMag); 
+		return distance.copy().magnitude(gravForceMag);
 	}
 	
 }
