722cd900041a6088b49649b7b869de49cf2dd977
yurysudz <yurysudz@gmail.com>
2015-02-09 16:16:32 -0800

    Basic physics groundwork
    Added Vector2 class that will be used for almost everything, and a
    physicsObject that I'm still not sure about how to implement.

--- src/PhysicsObject.java
+++ src/PhysicsObject.java
@@ -1,7 +1,41 @@
-
 public class PhysicsObject {
-
-	public PhysicsObject(){
-		
+	//A basic physics object
+	
+	
+	//Unfortunately, this is Java, so get/set methods for below vars
+	private Vector2 position;
+	private Vector2 velocity;
+	private Vector2 acceleration = new Vector2(0, 0);
+	
+	private double gravitationMass;
+	private double inertialMass;
+	
+	public PhysicsObject(Vector2 position, Vector2 velocity, double gravitationMass, double inertialMass){
+		this.position = position;
+		this.velocity = velocity;
+		this.gravitationMass  = gravitationMass;
+		this.inertialMass = inertialMass;
 	}
+	
+	public PhysicsObject(Vector2 position, double mass){
+		this(position, new Vector2(0, 0), mass, mass);
+	}
+	
+	public Vector2 calculateAcceleration(Vector2 force){
+		return force.multiply( 1 / inertialMass);
+	}
+	public void addForce(Vector2 force){
+		Vector2 deltaAcceleration = calculateAcceleration(force);
+		acceleration.setAdd(deltaAcceleration);
+	}
+	
+	public void update(){
+		velocity.setAdd(acceleration);
+		//So I reset acceleration every update cycle
+		//Is this how we should do it or store forces somehow or what?
+		acceleration.setEquals(new Vector2(0, 0));
+		position.setAdd(velocity);
+	}
+	
+	public Vector2 getPosition(){return position;}
 }
--- src/Vector2.java
+++ src/Vector2.java
@@ -1,33 +1,71 @@
-//basic Vector class with x and y values
-public class Vector2 {
-
-	//Should these be floats our doubles?
-	//How much precision do we need for every physics calculation?
+public class Vector2{
+	//Double Vector class with x and y
+	//
 	
-	public float x;
-	public float y;
+	//Add is like '+', setAdd is like '+='
 	
-	public Vector2(float x, float y){
+	public double x;
+	public double y;
+	
+	public Vector2(double x, double y){
 		this.x = x;
 		this.y = y;
 	}
 	
-	//Add is the equivalent of '+' on a vector, AddTo is '+='
-	public Vector2 add(Vector2 other){
-		return new Vector2(x + other.x, y + other.y);
-	}
-	public void addTo(Vector2 other){
-		x += other.x;
-		y += other.y;
+	public void setEquals(Vector2 a){
+		this.x = a.x;
+		this.y = a.y;
 	}
 	
-	public Vector2 subtract(Vector2 other){
-		return new Vector2(x - other.x, y - other.y);
-	}
-	public void subtractTo(Vector2 other){
-		x -= other.x;
-		y -= other.y;
+	public boolean equals(Vector2 a){
+		return (x == a.x && y == a.y);
 	}
 	
+	public Vector2 add(Vector2 b){
+		return new Vector2(x + b.x, y + b.y);
+	}
+	public void setAdd(Vector2 a){
+		this.setEquals(add(a));
+	}
+	
+	public Vector2 subtract(Vector2 b){
+		return new Vector2(x - b.x, y - b.y);
+	}
+	public void setSubtract(Vector2 a){
+		this.setEquals(subtract(a));
+	}
+	
+	public Vector2 multiply(Vector2 b){
+		return new Vector2(x * b.x, y * b.y);
+	}
+	public Vector2 multiply(double a){
+		return new Vector2(x * a, y * a);
+	}
+	public void setMultiply(Vector2 a){
+		this.setEquals(multiply(a));
+	}
+	public void setMultiply(double a){
+		this.setEquals(multiply(a));
+	}
+	
+	public double magnitude(){
+		return Math.sqrt(x * x + y * y);
+	}
+	public void setMagnitude(double mag){
+		double ratio = mag / magnitude();
+		setMultiply(ratio);
+	}
+	
+	//Sets the vector's magnitude to 1
+	public Vector2 normalize(){
+		Vector2 vec =  new Vector2(0, 0);
+		vec.setEquals(this);
+		vec.setMagnitude(1);
+		return vec;
+		
+	}
+	public void setNormalize(){
+		this.setEquals(normalize());
+	}
 	
 }
