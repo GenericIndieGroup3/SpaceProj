7113b5bb1de9b5d90787dbe056e2b51622cf7db6
rucraft42 <rucraft42@gmail.com>
2015-02-18 10:38:21 -0800

    Beginning of ship class

---
+++ src/obj/Gravitator.java
@@ -0,0 +1,24 @@
+package obj;
+
+import Physics.PhysicsObject;
+import Structs.Vector2;
+
+public class Gravitator extends PhysicsObject {
+	
+	public Gravitator(Vector2 position, Vector2 velocity, double gravitationalMass, double inertialMass){
+		super(position, velocity, gravitationalMass, inertialMass);
+	}
+	
+	public Gravitator(Vector2 position, double mass){
+		super(position, mass);
+	}
+	
+	public void addMass(double m){
+		this.gravitationalMass += m;
+	}
+	
+	public void setMass(double m){
+		this.gravitationalMass = m;
+	}
+	
+}
