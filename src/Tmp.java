8a9a8704c224b80058b99bd6b140cc8ebd92298c
rucraft42 <rucraft42@gmail.com>
2015-02-23 12:58:50 -0800

    a

---
+++ src/Tmp.java
@@ -0,0 +1,6 @@
+import org.lwjgl.input.Keyboard;
+public class Tmp {
+	public static void main(String[] args){
+		System.out.println(Keyboard.KEY_0);
+	}
+}
--- src/events/types/KeyPressEvent.java
+++ src/events/types/KeyPressEvent.java
@@ -4,5 +4,12 @@ import Physics.PhysicsObject;
 
 public class KeyPressEvent extends Event {
 	//TODO
+	private int key;
+	public KeyPressEvent(int key){
+		this.key = key;
+	}
 	
+	public int getKey(){
+		return key;
+	}
 }
