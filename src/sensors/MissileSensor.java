5e2665ba8491ecfe937efbeb0f57e8e585a8af3d
rucraft42 <rucraft42@gmail.com>
2015-03-05 12:41:06 -0800

    Sensors now work

--- src/sensors/MissileSensor.java
+++ src/sensors/MissileSensor.java
@@ -14,32 +14,30 @@ public class MissileSensor implements Sensor<Missile> {
 	private List<Missile> triggers = new ArrayList<Missile>();
 	private boolean isTriggered = false;
 	
-	private PhysicsSystem currentSystem;
-	
 	private PhysicsObject target;
 	public UUID targetId;
 	private double x;
 	private double y;
 	private double radius;
 	
-	public MissileSensor(PhysicsObject target, double radius, PhysicsSystem system){
+	public MissileSensor(PhysicsObject target, double radius){//, PhysicsSystem system){
 		this.target = target;
 		targetId = target.getUUID();
 		this.radius = radius;
-		currentSystem = system;
+		//currentSystem = system;
 	}
 	
-	public MissileSensor(double x, double y, double radius, PhysicsSystem system){
+	public MissileSensor(double x, double y, double radius){//, PhysicsSystem system){
 		this.x = x;
 		this.y = y;
 		this.radius = radius;
-		currentSystem = system;
+		//currentSystem = system;
 	}
 	
-	public MissileSensor(UUID id, double radius, PhysicsSystem system){
-		currentSystem = system;
+	public MissileSensor(UUID id, double radius){//, PhysicsSystem system){
+		//currentSystem = system;
 		this.targetId = id;
-		this.target = currentSystem.searchById(id);
+		this.target = MainGame.mainGame.getActiveSystem().getObject(id);
 		this.radius = radius;
 	}
 	
@@ -61,9 +59,9 @@ public class MissileSensor implements Sensor<Missile> {
 	public void update() {
 		triggers.clear();
 		isTriggered = false;
-		for(PhysicsObject p : currentSystem.objects){
+		for(PhysicsObject p : MainGame.mainGame.getActiveSystem().objects){
 			if(p instanceof Missile){
-				if((target != null && currentSystem.distanceTo(target, p) <= radius) || currentSystem.distanceTo(x,y,p) <= radius){
+				if((target != null && MainGame.mainGame.getActiveSystem().distanceTo(target, p) <= radius) || MainGame.mainGame.getActiveSystem().distanceTo(x,y,p) <= radius){
 					isTriggered = true;
 					triggers.add((Missile)p);
 				}
